3d7e3e163eaab2c724902d0a52c6c9842586f26f
Dan Simons <47121977+daandymaan@users.noreply.github.com>
2020-10-08 23:11:39 +0100

    Fixed the rings and the amount of prefabs per row

--- Game Engines Examples/Assets/Generator.cs
+++ Game Engines Examples/Assets/Generator.cs
@@ -6,11 +6,13 @@ public class Generator : MonoBehaviour
 {
     public int elements = 12;
     public float radius = 10;
+
+    public float rings = 9;
     public GameObject mp;
     // Start is called before the first frame update
     void Start()
     {
-        for (int j = 0; j < elements; j++)
+        for (int j = 0; j < rings; j++)
         {
             float theta = Mathf.PI * 2.0f / (float)(elements *j);
             for (int i = 0; i < (elements * j); i++)
