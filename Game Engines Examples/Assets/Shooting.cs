b3a92c3f4604ebce8dce708c999bbfa2cd08a9bf
Dan Simons <47121977+daandymaan@users.noreply.github.com>
2020-10-16 15:48:11 +0100

    Temp changes to shooting

--- Game Engines Examples/Assets/Shooting.cs
+++ Game Engines Examples/Assets/Shooting.cs
@@ -14,15 +14,25 @@ public class Shooting : MonoBehaviour
     {
         
     }
+    void Shoot(){
+        GameObject bullet = GameObject.Instantiate<GameObject>(bulletPrefab);
+        bullet.transform.position = spawnPoint.position;
+        bullet.transform.rotation = this.transform.rotation;
+    }
+
+    System.Collections.IEnumerator ShootingCoroutine() {
+        while(true){
+            if (Input.GetButton("Fire1")){
+                Shoot();
+                yield return new WaitForSeconds(1.0f/ (float)(fireRate));
+            }
+            yield return null;
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            GameObject bullet = GameObject.Instantiate<GameObject>(bulletPrefab);
-            bullet.transform.position = spawnPoint.position;
-            bullet.transform.rotation = this.transform.rotation;
-        }
+
     }
 }
